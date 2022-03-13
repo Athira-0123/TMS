62cb7304d59e3e3d8a04f0533ded14e154b723fb
Athira-0123 <93717827+Athira-0123@users.noreply.github.com>
2022-03-13 16:01:57 +0530

    Update Allocate.js

--- TMS-frontEnd/src/pages/Allocate.js
+++ TMS-frontEnd/src/pages/Allocate.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import '@fontsource/roboto/300.css';
 import AllocationTable from '../components/admin/AllocationTable';
-import Footer from '../components/footer/Footer';
+import Footer from '../components/footer';
 import AdminNav from '../components/navbar/AdminNav';
 
 function Allocate(props) {
