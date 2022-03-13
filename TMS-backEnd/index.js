d36a372e245d8877a4f230fa70577c8a10c5c89f
VimmySabu <vimmysabu1998@gmail.com>
2022-03-13 22:00:23 +0530

    pushing code

---
+++ TMS-backEnd/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const enrollment = require('./src/model/EnrollmentModel');
+const enrollmentRouter = require('./src/routes/EnrollmentRoute');
+const adminRouter = require('./src/routes/AdminRoute');
+const accountsRouter = require("./src/routes/userRouter");
+const trainerRouter=require('./src/routes/TrainerRoute')
+const auth = require ("./src/utility/auth");
+
+const path = require('path');
+
+
+
+const app = express();
+app.use(cors());
+// Post Method
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./build/'));
+
+
+
+
+
+
+
+app.use('/api/enrollments',enrollmentRouter);
+app.use('/api/admin',adminRouter) 
+app.use("/api/user", accountsRouter);
+app.use('/api/trainer',auth,trainerRouter)
+
+
+
+
+
+
+
+app.get('/*',function(req,res){
+     res.sendFile(path.join(__dirname+'/build/index.html'));
+ });
+
+
+
+
+
+app.listen(process.env.PORT || 5001,()=>{
+    console.log("Server Ready on 5001"); 
+});
---
+++ TMS-frontEnd/src/pages/EnrollPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import Sidebar from '../components/Sidebar';
+import CreateEnrollment from '../components/enrollment/CreateEnrollment';
+import Footer from '../components/footer';
+
+const EnrollPage = () => {
+    return (
+        
+    <>
+    <Navbar/>
+    <Sidebar/>
+    <CreateEnrollment/>
+    <Footer/>
+    </>
+
+
+    )
+}
+
+export default EnrollPage
