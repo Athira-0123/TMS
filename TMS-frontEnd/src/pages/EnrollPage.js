b10d25d9d23ac22e397834c9c6a03eee1b1fb352
amalkdl007 <93526375+amalkdl007@users.noreply.github.com>
2022-03-14 21:38:06 +0530

    Toggle Added

--- TMS-frontEnd/src/pages/EnrollPage.js
+++ TMS-frontEnd/src/pages/EnrollPage.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/Sidebar";
 import CreateEnrollment from "../components/enrollment/CreateEnrollment";
 import Footer from "../components/footer";
 
 const EnrollPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
       <CreateEnrollment />
       <Footer />
     </>
--- TMS-frontEnd/src/pages/LoginPage.js
+++ TMS-frontEnd/src/pages/LoginPage.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/Sidebar";
 import Login from "../components/login/Login";
 import Footer from "../components/footer";
 
 const LoginPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <Navbar />
-      <Sidebar />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
       <Login />
       <Footer />
     </>
