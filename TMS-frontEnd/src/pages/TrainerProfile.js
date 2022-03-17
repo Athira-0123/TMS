bee424d7fb264af4c53b7bf81a7ad0ae605c480b
ArchanaPalliyalil <archanapalliyalil@gmail.com>
2022-03-17 09:50:19 +0530

    added profile

--- TMS-frontEnd/src/pages/TrainerProfile.js
+++ TMS-frontEnd/src/pages/TrainerProfile.js
@@ -1,8 +1,8 @@
 // Trainer Profile
-/*
+
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import "./profile.css";
+import "./editprofile.css";
 function TrainerProfile(props) {
   const navigation = useNavigate();
 
@@ -64,7 +64,7 @@ function TrainerProfile(props) {
 
   return (
     <>
-      <div className="wrapper">
+      <div className="wrapperedit">
         <div className=" inner innerprofile">
           <form>
             <h3>My Profile</h3>
@@ -205,153 +205,5 @@ function TrainerProfile(props) {
   );
 }
 export default TrainerProfile;
-*/
-
-// Trainer Profile
-import { borderRadius } from "@mui/system";
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import "./profile.css";
 
 
-function TrainerProfile(props) {
-  const navigation = useNavigate();
-
-  const { id } = useParams();
-
-  useEffect(() => {
-    fetchAPI();
-  }, []);
-  async function fetchAPI() {
-    const response = await fetch(`http://localhost:5001/api/trainer/${id}`, {
-      headers: {
-        "x-access-token": localStorage.getItem("hornbill"),
-      },
-    });
-    const body = await response.json();
-    console.log(body);
-    setTrainerValues(body);
-  }
-  // Storing trainer details
-  const [trainerValues, setTrainerValues] = useState({
-    _id: "",
-    first_name: "",
-    last_name: "",
-    gender: "",
-    date_of_birth: "",
-    address_line_1: "",
-    address_line_2: "",
-    city: "",
-    district: "",
-    state: "",
-    zip_Code: "",
-    phone: "",
-    email_address: "",
-    password: "",
-    confirmpassword: "",
-    highest_qualification: "",
-    skill_set: "",
-    current_company_name: "",
-    current_designation: "",
-    ictak_course_handling: "",
-  });
-
-  const handleSubmit = (e, trainerValues) => {
-    e.preventDefault();
-
-    navigation(`/trainer/edit/${trainerValues.id}`, { replace: true });
-  };
-
-  var base64String = "";
-  if (trainerValues.image !== undefined) {
-    try {
-      base64String = btoa(
-        new Uint8Array(trainerValues.image.data.data).reduce(function (
-          data,
-          byte
-        ) {
-          return data + String.fromCharCode(byte);
-        },
-        "")
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  
-  return (
-    <>
-       <div class="wrapper">
-        <div class="sidebar">
-          <form onSubmit={handleSubmit}>
-            <img
-              className="imageprofile"
-              style={{
-                width: 150,
-                height: 150,
-                resizeMode: "cover",
-                backgroundColor: "white",
-              }}
-              src={`data:image/png;base64,${base64String}`}
-              alt=""/>
-         
-           
-            <p className="name">{trainerValues.first_name} &nbsp;
-              {trainerValues.last_name}<br></br>
-               Course Handling : {trainerValues.ictak_course_handling}</p>
-          </form>
-        </div>
-        </div>
-
-        
-        <div class="wrapper">
-        <div class="container">
-          <div class="header">
-            <h3>Details</h3>
-          </div>
-          <form onSubmit={handleSubmit}>
-         
-              <p className="errorprofile"> 
-                Email : {trainerValues.email_address} &nbsp; &nbsp;   Phone No: {trainerValues.phone}
-                &nbsp; &nbsp;  Date of Birth: {trainerValues.date_of_birth}
-              <br></br>Skill : {trainerValues.skill_set} &nbsp; &nbsp; 
-              Address : {trainerValues.address_line_1} {trainerValues.address_line_2} <br></br>
-              City : {trainerValues.city} &nbsp; &nbsp; District :{trainerValues.district} &nbsp; &nbsp;
-              State : {trainerValues.state} &nbsp; &nbsp; Zip Code : {trainerValues.zip_Code} <br></br>
-              Experience : {trainerValues.current_designation} &nbsp; , {trainerValues.current_company_name}
-              
-              </p>
-           
-            <div className="btns">
-              <button class="alink" value="Submit application">
-                <Link to={`/trainer/edit/${trainerValues._id}`}>Edit</Link>
-              </button>
-              <br />
-              
-            </div>
-          </form>
-        </div>
-        </div>
-
-
-        <div class="wrapper">
-    <div class="horizontal">
-      <div class="header">
-        <h4 class="hh">My Allocations</h4>
-      </div>
-      <form onSubmit={handleSubmit}>
-  <div class="column"></div>
-          <div className="btns">
-              <button class="alink" >
-                <Link to="TrainerAllocation">view</Link>
-              </button>
-            </div>
-      </form>
-    </div>
-    </div>
-    </>
-  );
-}
-export default TrainerProfile
-;
